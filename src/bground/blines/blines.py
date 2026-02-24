15837f74889234e708d68278b820eda0c05af8b6
Jakub David <jakubdavid64@gmail.com>
2026-02-24 21:34:14 +0100

    Baselines implementation

--- src/bground/blines/blines.py
+++ src/bground/blines/blines.py
@@ -16,3 +16,60 @@ Alternative input/output:
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def select_xrange(x: np.ndarray, y: np.ndarray, xrange: tuple[int, int]):
+    '''Select data from a range on the x axis.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        X axis of the data.
+    y : np.ndarray
+        Y axis of the data.
+    xrange : tuple[int, int]
+        A range on the x axis to be selected.
+        The range is inclusive.
+
+    Returns
+    -------
+    tuple
+        Arrays x and y in the specified xrange.
+    '''
+    start, end = xrange
+    mask = (x >= start) & (x <= end)
+    return x[mask], y[mask]
+
+def subtract_baseline(x: np.ndarray, y: np.ndarray, baseline: np.ndarray, xrange: tuple[int, int]):
+    '''Subtract baseline from XY-data.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        X axis of the data.
+    y : np.ndarray
+        Y axis of the data.
+    baseline : np.ndarray
+        Baseline to be subtracted.
+    xrange : tuple[int, int]
+        A range on the x axis, where the baseline is defined.
+        The range is inclusive.
+
+    Returns
+    -------
+    np.ndarray
+        Array with 4 rows `[X, Iraw, Ibkg, I = Iraw - Ibkg]`.
+    '''
+
+
+    start, end = xrange
+
+    bground_mask = (x >= start) & (x <= end)
+    baseline_full = np.zeros_like(y)
+    baseline_full[bground_mask] = baseline
+
+    return np.stack((
+        x,
+        y,
+        baseline_full,   
+        np.where(bground_mask, y - baseline_full, 0)
+    ))
